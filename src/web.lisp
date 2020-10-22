4338e0621f0c381a2c9c3e6d31f25214be1a71f3
Dz0526 <shining.sun0526@gmail.com>
2020-10-22 20:27:09 +0900

    complete inc dec (DB html)

--- src/web.lisp
+++ src/web.lisp
@@ -24,12 +24,53 @@
 ;; Routing rules
 
 (defroute "/" ()
-  (render #P"index.html"))
+  (render-people))
+
+(defroute ("/increment" :method :GET ) ()
+  (increment-person)
+  (render-people))
+
+(defroute ("/decrement" :method :GET) ()
+  (decrement-person)
+  (render-people))
 
 ;;
 ;; Error pages
+
+;;
+;; Render
+
+(defun render-people ()
+  (render #P"index.html" `(:numbers ,(send-data))))
+
+(defun send-data ()
+  (with-connection (db)
+    (retrieve-all
+      (select :* (from :person)))))
 
 (defmethod on-exception ((app <web>) (code (eql 404)))
   (declare (ignore app))
   (merge-pathnames #P"_errors/404.html"
                    *template-directory*))
+
+(defun increment-person ()
+  (with-connection (db)
+    (let ((num (retrieve-one (select :number
+                               (from :person)
+                               (where (:= :id 1))))) ) 
+      (execute
+        (update :person
+          (set= :number (1+ (cadr num)))
+          (where (:= :id 1))))  )))
+
+(defun decrement-person ()
+  (with-connection (db)
+    (let ((num (retrieve-one (select :number
+                               (from :person)
+                               (where (:= :id 1))))) ) 
+      (format t "うんこ ~d" 256)
+      (format t " ~d" (cadr num ) )
+      (execute
+        (update :person
+          (set= :number (1- (cadr num )))
+          (where (:= :id 1))))  )))
